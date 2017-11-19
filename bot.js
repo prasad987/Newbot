9ba65c278eda5902b2700c71c86f6ceb1a900725
TheNaman047 <namankhandelwal314@gmail.com>
2017-11-19 23:36:28 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,5 @@
 var Discord = require('discord.js')
-var logger = require('winston');
-
-bot.login(process.env.BOT_TOKEN);
+var bot = new Discord.Client();
 
 // Variables
 var getNotes = "";
@@ -25,24 +23,10 @@ fs.readFile(filePath, { encoding: 'utf-8' }, function (err, data) {
     }
 });
 
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(logger.transports.Console, {
-    colorize: true
-});
-logger.level = 'debug';
+bot.login(process.env.BOT_TOKEN);
 
-// Initialize Discord Bot
-var bot = new Discord.Client({
-    token: auth.token,
-    autorun: true
-});
-
-//when the bot executes
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
+bot.on('ready', () => {
+    console.log("Connected");
 });
 
 bot.on('message', message => {
@@ -58,101 +42,106 @@ bot.on('message', message => {
                 case "$showProgress":
                     message.channel.send("Needs to be decided !!!");
 
-                    log = log + "\n" + auther + " did " + message + "\n";
+                    log = log + "\n" + auther + " did " + msg + "\n";
                     break;
 
                 case "$showAccomplished":
-                    message.channel.send(getAccomplishedData);
-                    
-                    log = log + "\n" + auther + " did " + message + "\n";
+                    if (getAccomplishedData == "") message.channel.send("Nothing Yet !");
+                    else message.channel.send(getAccomplishedData);
+
+                    log = log + "\n" + auther + " did " + msg + "\n";
                     break;
 
                 case "$clearAccomplished":
                     getAccomplishedData = "";
 
                     message.channel.send("CLEARED !!!");
-                    
-                    log = log + "\n" + auther + " did " + message + "\n";
+
+                    log = log + "\n" + auther + " did " + msg + "\n";
                     break;
 
                 case "$showNext":
-                message.channel.send(addNextData);
-                    
-                    log = log + "\n" + auther + " did " + message + "\n";
+                    if (addNextData == "") message.channel.send("Nothing Yet !");
+                    else message.channel.send(addNextData);
+
+                    log = log + "\n" + auther + " did " + msg + "\n";
                     break;
 
                 case "$clearNext":
-                
+
                     addNextData = "";
 
                     message.channel.send("CLEARED !!!");
-                   
-                    log = log + "\n" + auther + " did " + message + "\n";
+
+                    log = log + "\n" + auther + " did " + msg + "\n";
                     break;
 
                 case "$showNotes":
-                message.channel.send(getNotes);
-                    
-                    log = log + "\n" + auther + " did " + message + "\n";
+                    if (getNotes == "") message.channel.send("Nothing Yet !");
+                    else message.channel.send(getNotes);
+
+                    log = log + "\n" + auther + " did " + msg + "\n";
                     break;
 
                 case "$clearNotes":
-                
+
                     getNotes = "";
 
                     message.channel.send("CLEARED !!!");
 
-                    log = log + "\n" + auther + " did " + message + "\n";
+                    log = log + "\n" + auther + " did " + msg + "\n";
                     break;
 
                 case "$getLogs":
-                message.channel.send("Here are the logs: \n" + log);
-                    
-                break;
+                    message.channel.send("Here are the logs: \n" + log);
+
+                    break;
 
                 case "$help":
-                message.channel.send("Here you go " + auther + "\n\n" + helpMessage);
-                    
-                    log = log + "\n" + auther + " did " + message + "\n";
+                    message.channel.send("Here you go " + auther + "\n\n" + helpMessage);
+
+                    log = log + "\n" + auther + " did " + msg + "\n";
                     break;
 
                 default:
-                    message.delete();
-                    message.channel.send("Seems that this commands is not available. To see available commands type $help and available list of commands wil be presented.")
-                    break;
-            }
+                    if (msg.substr(0, 8) == "$addNext") {
+                        addNextData = addNextData + "\n" + msg.substring(9, msg.length);
 
-            if(message.substr(0, 8) == "$addNext"){
-                addNextData = addNextData + "\n" + message.substring(9, message.length);
-        
-                message.channel.send("NOTED !!!");
-        
-                log = log + "\n" + auther + " did " + message + "\n";
-            }
-        
-            if(message.substr(0, 19) == "$updateAccomplished"){
-                getAccomplishedData = getAccomplishedData + "\n" + message.substring(20, message.length);
-        
-                message.channel.send("NOTED !!!");
-        
-                log = log + "\n" + auther + " did " + message + "\n";
-            }
-        
-            if(message.substr(0, 9) == "$addNotes"){
-                getNotes = getNotes + "\n" + message.substring(10, message.length);
-        
-                message.channel.send("NOTED !!!");
-        
-                log = log + "\n" + auther + " did " + message + "\n";
-            }
-        
-            if(log.includes("undefined")){
-                log.replace("undefined", "");
+                        message.channel.send("NOTED !!!");
+
+                        log = log + "\n" + auther + " did " + msg + "\n";
+                    }
+
+                    else if (msg.substr(0, 19) == "$updateAccomplished") {
+                        getAccomplishedData = getAccomplishedData + "\n" + msg.substring(20, msg.length);
+
+                        message.channel.send("NOTED !!!");
+
+                        log = log + "\n" + auther + " did " + msg + "\n";
+                    }
+
+                    else if (msg.substr(0, 9) == "$addNotes") {
+                        getNotes = getNotes + "\n" + msg.substring(10, msg.length);
+
+                        message.channel.send("NOTED !!!");
+
+                        log = log + "\n" + auther + " did " + msg + "\n";
+                    }
+
+                    else {
+                        message.delete();
+                        message.channel.send("Seems that this commands is not available. To see available commands type $help and available list of commands wil be presented.")
+                    }
+
+                    if (log.includes("undefined")) {
+                        log.replace("undefined", "");
+                    }
+
+                    break;
             }
         }
         else {
             message.channel.send(sender + " this bot only accepts commands that start with '$'. Use $help to see available commands")
         }
     }
-
-})
+});

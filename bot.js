b5dbae6e1b81a153d12d897acf3544ae4993ba0d
prasad987 <54022358+prasad987@users.noreply.github.com>
2019-08-13 15:20:19 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,25 +33,25 @@ bot.on('message', message => {
     // Variables
     var sender = message.author; //The person who sent the message
     var msg = message.content;
-    var prefix = "$" //The text before commands, you can put anything that you prefer
+    var prefix = "!" //The text before commands, you can put anything that you prefer
 
    
-        if (msg.startsWith('$', 0)) {
+        if (msg.startsWith('!', 0)) {
             switch (msg) {
-                case "$showProgress":
+                case "!showProgress":
                     message.channel.send("Needs to be decided !!!");
 
                     log = log + "\n" + sender + " did " + msg + "\n";
                     break;
 
-                case "$showvnkey":
+                case "!showvnkey":
                     if (getGuidData == "") message.channel.send("Nothing Yet !");
                     else message.channel.send(getGuidData);
 
                     log = log + "\n" + sender + " did " + msg + "\n";
                     break;
 
-                case "$clearvnkey":
+                case "!clearvnkey":
                     getGuid = "";
 
                     message.channel.send("CLEARED !!!");
@@ -59,14 +59,14 @@ bot.on('message', message => {
                     log = log + "\n" + sender + " did " + msg + "\n";
                     break;
 
-                case "$showNext":
-                    if (addNextData == "") message.channel.send("Nothing Yet !");
+                case "!hi":
+                    if (addNextData == "") message.channel.send(" + sender + ""hello!");
                     else message.channel.send(addNextData);
 
                     log = log + "\n" + sender + " did " + msg + "\n";
                     break;
 
-                case "$clearNext":
+                case "!clearNext":
 
                     addNextData = "";
 
@@ -75,14 +75,14 @@ bot.on('message', message => {
                     log = log + "\n" + sender + " did " + msg + "\n";
                     break;
 
-                case "$showGuid":
+                case "!showGuid":
                     if (getNotes == "") message.channel.send("Nothing Yet !");
                     else message.channel.send(getNotes);
 
                     log = log + "\n" + sender + " did " + msg + "\n";
                     break;
 
-                case "$clearGuid":
+                case "!clearGuid":
 
                     getNotes = "";
 
@@ -91,27 +91,27 @@ bot.on('message', message => {
                     log = log + "\n" + sender + " did " + msg + "\n";
                     break;
 
-                case "$getLogs":
+                case "!getLogs":
                     message.channel.send("Here are the logs: \n" + log);
 
                     break;
 
-                case "$help":
+                case "!help":
                     message.channel.send("Here you go " + sender + "\n\n" + helpMessage);
 
                     log = log + "\n" + sender + " did " + msg + "\n";
                     break;
 
                 default:
-                    if (msg.substr(0, 8) == "$addNext") {
-                        addNextData = addNextData + "\n" + msg.substring(9, msg.length);
+                    if (msg.substr(0, 8) == "!addNext") {
+                        addNextData = addNextData + "\n" + msg.substring(3, msg.length);
 
                         message.channel.send("NOTED !!!");
 
                         log = log + "\n" + sender + " did " + msg + "\n";
                     }
 
-                    else if (msg.substr(0, 12) == "$updatevnkey") {
+                    else if (msg.substr(0, 12) == "!updatevnkey") {
                         getGuidData = getGuidData + "\n" + msg.substring(13, msg.length);
 
                         message.channel.send("NOTED !!!");
@@ -119,7 +119,7 @@ bot.on('message', message => {
                         log = log + "\n" + sender + " did " + msg + "\n";
                     }
 
-                    else if (msg.substr(0, 8) == "$addGuid") {
+                    else if (msg.substr(0, 8) == "!addGuid") {
                         getNotes = getNotes + "\n" + msg.substring(10, msg.length);
 
                         message.channel.send("NOTED !!!");
